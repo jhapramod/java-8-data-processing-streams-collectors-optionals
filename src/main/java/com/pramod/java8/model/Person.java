6461d8794eb3297b1949aff042dfd8567ea6caf8
SG0301769 <pramod.kumar@sabre.com>
2020-05-23 19:15:12 +0530

    Predicate and Functional Interface Example

--- src/main/java/com/pramod/java8/model/Person.java
+++ src/main/java/com/pramod/java8/model/Person.java
@@ -2,9 +2,11 @@ package com.pramod.java8.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Person {
 	private String name;
 	private int age;
